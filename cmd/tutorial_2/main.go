f27cd5928c19e350c2a13586ce9c17d0e2b593b5
abhiram-ar <abhiram2001.ars@gmail.com>
2025-07-13 19:46:24 +0530

    data types

--- cmd/tutorial_2/main.go
+++ cmd/tutorial_2/main.go
@@ -1,4 +1,20 @@
 package main
 
+import "fmt"
+
 func main() {
+	var a int8 = 10
+	fmt.Println(a)
+
+	// var b int8 = 1001313
+	// fmt.Println(b) // this throw error as int8 cannnot hold such a large number
+
+	var str string = "abhiram"
+	fmt.Println(str)
+
+	str2 := "short declaration operator, intertype from right hand side"
+	fmt.Println(str2)
+
+	r := '😑' // go uses utf-8 for string encoding, to handle unicode wich can have more than one byte per character we use runes,
+	fmt.Println(r)
 }
